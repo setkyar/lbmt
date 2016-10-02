a9fbe50e7e96246921f2b00e63373149448bb9e6
Set Kyar Wa Lar <setkyar16@gmail.com>
2016-10-02 16:11:02 +0630

    initial commit

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,11 @@
+@extends('app')
+
+@section('content')
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-md-12">
+			<h1>This is just a test</h1>
+		</div>
+	</div>
+</div>
+@endsection
